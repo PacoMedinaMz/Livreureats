ec198c173696f1adbeafbc535664e85d5b4c7d49
Rodrigo Maafs Atilano <rodrigoelmaps@gmail.com>
2021-06-11 22:02:54 -0500

    Poder enviar/recibir mensajes al servidor socket

--- screens/Pantallas/Opciones.tsx
+++ screens/Pantallas/Opciones.tsx
@@ -3,6 +3,15 @@ import { ScrollView } from "react-native";
 import { View, Text, StyleSheet } from "react-native";
 import InputMessage from "../../components/Chat/InputMessage";
 import Mensaje from "../../components/Chat/Mensaje";
+import io from "socket.io-client";
+
+/*declare var io: {
+  connect(url: string): Socket;
+};
+interface Socket {
+  on(event: string, callback: (data: any) => void): void;
+  emit(event: string, data: any): void;
+}*/
 
 let mensajes: any = [
   {
@@ -15,89 +24,45 @@ let mensajes: any = [
     texto: "Gracias",
     fecha: "9:27 p.m",
   },
-  {
-    autor: "Administrador",
-    texto: "Muy bien Rodrigo. Si gustas algo no olvides contactarnos.",
-    fecha: "9:27 p.m",
-  },
-  {
-    autor: "Rodrigo",
-    texto: "Nada solamente estoy viendo",
-    fecha: "9:27 p.m",
-  },
-  {
-    autor: "Administrador",
-    texto: "Buen día. ¿En qué te podemos ayudar?",
-    fecha: "9:27 p.m",
-  },
-  {
-    autor: "Rodrigo",
-    texto: "Hola soy un mensaje",
-    fecha: "9:27 p.m",
-  },
-  {
-    autor: "Administrador",
-    texto: "Para servirte.",
-    fecha: "9:27 p.m",
-  },
-  {
-    autor: "Rodrigo",
-    texto: "Gracias",
-    fecha: "9:27 p.m",
-  },
-  {
-    autor: "Administrador",
-    texto: "Muy bien Rodrigo. Si gustas algo no olvides contactarnos.",
-    fecha: "9:27 p.m",
-  },
-  {
-    autor: "Rodrigo",
-    texto: "Nada solamente estoy viendo",
-    fecha: "9:27 p.m",
-  },
-  {
-    autor: "Administrador",
-    texto: "Buen día. ¿En qué te podemos ayudar?",
-    fecha: "9:27 p.m",
-  },
-  {
-    autor: "Rodrigo",
-    texto: "Hola soy un mensaje",
-    fecha: "9:27 p.m",
-  },
-  {
-    autor: "Administrador",
-    texto: "Para servirte.",
-    fecha: "9:27 p.m",
-  },
-  {
-    autor: "Rodrigo",
-    texto: "Gracias",
-    fecha: "9:27 p.m",
-  },
-  {
-    autor: "Administrador",
-    texto: "Muy bien Rodrigo. Si gustas algo no olvides contactarnos.",
-    fecha: "9:27 p.m",
-  },
-  {
-    autor: "Rodrigo",
-    texto: "Nada solamente estoy viendo",
-    fecha: "9:27 p.m",
-  },
-  {
-    autor: "Administrador",
-    texto: "Buen día. ¿En qué te podemos ayudar?",
-    fecha: "9:27 p.m",
-  },
-  {
-    autor: "Rodrigo",
-    texto: "Hola soy un mensaje",
-    fecha: "9:27 p.m",
-  },
 ];
 
 const Opciones = () => {
+  const [mensajes, setMensajes] = React.useState([]);
+  const [socket, setSocket] = React.useState<any>({});
+
+  React.useEffect(() => {
+    //Nos conectamos al socket
+    let sock: any = io("http://192.168.1.100:3000");
+    setSocket(sock);
+
+    //Al recibir algún mensaje...
+    sock.on("messages", (msg: any) => {
+      setMensajes(msg);
+    });
+  }, []);
+
+  function submitChatMessage(text: string) {
+    //Creamos el objeto que será enviado al socket.
+    let msg = {
+      autor: "React", //Por esto salía undefined
+      texto: text,
+      fecha: getDateFormat(),
+    };
+    socket.emit("new-message", msg);
+  }
+
+  function getDateFormat() {
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes: any = date.getMinutes();
+    let ampm = hours >= 12 ? "pm" : "am";
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    var strTime = hours + ":" + minutes + " " + ampm;
+    return strTime;
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.stack}>
@@ -105,11 +70,7 @@ const Opciones = () => {
       </View>
 
       <View style={{ width: "100%", height: 64 }}>
-        <InputMessage
-          onSubmit={(text: string) => {
-            console.log(text);
-          }}
-        />
+        <InputMessage onSubmit={(text: string) => submitChatMessage(text)} />
       </View>
 
       <ScrollView
@@ -119,6 +80,7 @@ const Opciones = () => {
       >
         {mensajes &&
           mensajes.map((msg: any, i: number) => {
+            console.log("Ciclo:", msg);
             return [<Mensaje mensaje={msg} key={i} />];
           })}
       </ScrollView>
